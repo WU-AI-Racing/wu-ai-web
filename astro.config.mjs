cc9b994e39b20c0d8db833cec74acf444c0bdad2
mandmliketherapper <m.rodriguez-montoya@wustl.edu>
2025-11-07 21:08:54 -0600

    maybe fixed apple iphone issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      host: true,              // allows all network connections
+      allowedHosts: ['all'],   // accept ngrok / external URLs
+      port: 4321,              // optional, but good to fix the port
+    },
+  },
+});
